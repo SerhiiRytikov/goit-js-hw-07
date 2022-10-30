bdee30084f6835d6cad3a4121fbb51c271fca76b
SerhiiRytikov <serg_ritikov@ukr.net>
2022-10-30 16:58:09 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulImages = document.querySelector(".gallery")
+const createGallery = (galleryItems) => {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <ul class="gallery">
+            <li class="gallery__item">
+                <a class="gallery__link"
+                 href="${original}">
+                    <img class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}">
+                </a>
+            </li>
+        </ul>
+        `
+    })
+}
