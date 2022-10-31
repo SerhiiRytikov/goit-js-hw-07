c383ffbe30e7beaf6e7c77a8214ac10c3955579a
SerhiiRytikov <serg_ritikov@ukr.net>
2022-10-31 13:02:33 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,19 +4,25 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const ulImages = document.querySelector(".gallery")
+
 const createGallery = (galleryItems) => {
     return galleryItems.map(({preview, original, description}) => {
-        return `
-        <ul class="gallery">
+        return`
+        
             <li class="gallery__item">
                 <a class="gallery__link"
                  href="${original}">
                     <img class="gallery__image"
-                    src="${preview}" 
+                    src="${preview}"
+                    data-source="${original}"
                     alt="${description}">
                 </a>
             </li>
-        </ul>
+        
         `
+        
     })
+    .join("")
 }
+
+ulImages.insertAdjacentHTML("afterbegin", createGallery(galleryItems))
