fdf15731f010a49f4c5802ed69130c440def8fe7
SerhiiRytikov <serg_ritikov@ukr.net>
2023-01-31 16:43:41 +0200

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,22 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryAllPhotoPreview = document.querySelector('.gallery');
 
-const galleryItem = document.querySelector(".gallery");
+const gelImg = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+    </a>`,
+  )
+  .join('');
 
-function createGallery(item) {
-  return item
-    .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href=${original}>
-  <img class="gallery__image" src=${preview} alt=${description} />
-</a>`;
-    })
-    .join("");
-}
-galleryItem.innerHTML = createGallery(galleryItems);
+galleryAllPhotoPreview.innerHTML = gelImg;
 
-let gallery = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
   captionDelay: 250,
 });
